de5ce6021058d3972b430515b8bba1e1dccc5ea7
vinariesandi <vinariesandi@gmail.com>
2018-03-25 22:49:54 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/user/vinaaiesandi_1202140178_modul5/AddData.java
@@ -0,0 +1,44 @@
+package com.example.user.vinaaiesandi_1202140178_modul5;
+
+/**
+ * Created by Lenovo on 24/03/2018.
+ */
+
+public class AddData {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior) {
+        this.todo = todo;
+        this.desc = desc;
+        this.prior = prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
+
+
